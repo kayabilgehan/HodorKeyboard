7f863a95a576b3d550a0496bc8897abe7e42e467
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-05-21 12:40:03 +0300

    Project Finished

--- HodorKeyboard/ViewController.swift
+++ HodorKeyboard/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  HodorKeyboard
-//
-//  Created by Bilgehan KAYA on 20.05.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
